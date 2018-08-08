ab695456fefa6788f176a28a53a5a3d8382a717b
Vinod  Vanga <vinod.vanga@imaginea.com>
2018-08-08 12:17:34 +0530

    Update repository with code

---
+++ Code/Pramati_EML_Reader/EML.Activities/ParseEML.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using System.IO;
+
+namespace Pramati.EML.Activities
+{
+    public class ParseEML : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> FilePath { get; set; }
+
+
+        [Category("Output")]
+        public OutArgument<MsgReader.Mime.Message> MailObject { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> Message { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string filePath = FilePath.Get(context);
+            MsgReader.Mime.Message mailObject = null;
+            string message = null;
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    if (Path.GetExtension(filePath).ToLower() == ".eml")
+                    {
+                        Stream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+                        mailObject = MsgReader.Mime.Message.Load(stream);
+                       
+                        MailObject.Set(context, mailObject);
+                        stream.Dispose();
+                       
+                        if (MailObject != null)
+                        {
+                            message = "Success";
+                          
+                        }
+                    }
+                }
+                else
+                {
+                    message = "File doesnot exist";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                message = ex.Message;
+            }
+            finally
+            {
+               
+                Message.Set(context, message);
+                mailObject = null;
+            }
+        }
+    }
+}
---
+++ Code/Pramati_EML_Reader/EML.Activities/ParseMSG.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using System.IO;
+
+namespace Pramati.EML.Activities
+{
+    //public class ParseMSG : CodeActivity
+    //{
+    //    [Category("Input")]
+    //    [RequiredArgument]
+    //    public InArgument<string> FilePath { get; set; }
+
+
+    //    [Category("Output")]
+    //    public OutArgument<MsgReader.Outlook.Storage.Message> MailObject { get; set; }
+
+    //    [Category("Output")]
+    //    public OutArgument<string> Message { get; set; }
+
+    //    protected override void Execute(CodeActivityContext context)
+    //    {
+    //        string filePath = FilePath.Get(context);
+    //        MsgReader.Outlook.Storage.Message mailObject = null;
+    //        string message = null;
+
+
+    //        string fileExtension = Path.GetExtension(filePath);
+    //        try
+    //        {
+    //            if (File.Exists(filePath))
+    //            {
+    //                if (fileExtension.ToLower() == ".msg")
+    //                {
+
+    //                    mailObject = new MsgReader.Outlook.Storage.Message(filePath);
+    //                    MailObject.Set(context, mailObject);
+    //                    if (mailObject != null)
+    //                    {
+    //                        message = "Success";
+    //                    }
+
+
+    //                }
+    //            }
+    //            else
+    //            {
+    //                message = "File doesnot exist";
+    //            }
+
+
+
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            message = ex.Message;
+    //        }
+    //        finally
+    //        {
+    //            Message.Set(context, message);
+    //        }
+    //    }
+    //}
+}
